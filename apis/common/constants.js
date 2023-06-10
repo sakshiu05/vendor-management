783d5c62a7d453095b828713c66ec46bc1561694
sakshiu05 <135985596+sakshiu05@users.noreply.github.com>
2023-06-10 13:30:19 +0530

    Update code

--- apis/common/constants.js
+++ apis/common/constants.js
@@ -1,8 +1,8 @@
 module.exports = {
-    aws_key_id: 'AKIAQPB36OTH7ZAZUJON',
-    aws_secret: '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',
-    aws_region: 'ap-south-1',
-    // sendgrid: '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',
-    client_id:'2a4bab5a1610e8b08cde639077d3df53f36fa47ab5b5.api.hackerearth.com',
-    client_secret: '4df7c89129fe3f8b86f79f1976e18011f533f7ee',
+    aws_key_id: '',
+    aws_secret: '',
+    aws_region: '',
+    // sendgrid: '',
+    client_id:'',
+    client_secret: '',
 };
